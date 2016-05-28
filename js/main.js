0ba860f3d3b0ed3d767c852b35bdef650d7625bb
Matthew Kolodziejczak <makolodzi@gmail.com>
2016-05-28 12:01:43 -0400

    Store display number when operator is clicked

--- js/main.js
+++ js/main.js
@@ -6,11 +6,14 @@ var model = {
 	// refers to every number that is not the total
 	displayNumber: "",
 
+	// refers to a temporary stored number when an operator is clicked
+	storeNumber: "",
+
 	// refers to the current sum or total
 	displayTotal: 0,
 
 	// refers to operators +, -, *, and /
-	displayOperator: null
+	displayOperator: null,
 
 };
 
@@ -29,12 +32,23 @@ var controller = {
 	setDisplayNumber: function(num){
 		if (model.displayNumber.length === 15){
 			return;
+		} else if(num === ""){
+			model.displayNumber = "";
 		} else {
 			model.displayNumber += num;
 		}
 	},
 
+	storeNumber: function(num){
+		model.storeNumber = num;
+	},
+
 	setOperator: function(operator){
+		var number = this.getDisplayNumber();
+		if(number.length > 0){
+			this.storeNumber(number);
+			this.setDisplayNumber("");
+		}
 		model.displayOperator = operator;
 	},
 
